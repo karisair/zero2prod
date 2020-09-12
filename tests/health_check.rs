7c779703c19530e819dacdfaeb7113c54022a29e
Chris Dolby <csdolby@gmail.com>
2020-09-12 12:28:18 +0100

    Fix test macros - not present in actix_web but actix_rt as before

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -9,7 +9,7 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
-#[actix_web::test]
+#[actix_rt::test]
 async fn health_check_works() {
     // Arrange
     let app = spawn_app().await;
@@ -68,7 +68,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     connection_pool
 }
 
-#[actix_web::test]
+#[actix_rt::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
@@ -96,7 +96,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(saved.name, "le guin");
 }
 
-#[actix_web::test]
+#[actix_rt::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
     let app = spawn_app().await;
